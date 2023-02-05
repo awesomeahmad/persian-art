98d336361cad35c9abcc6f1612ef594e651707f6
Ahmad <ahmadhabibi877@yahoo.com>
2023-02-05 23:22:27 +0330

    add axios n modified app to fetch products

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,28 @@
 
 //import 'bootstrap/dist/css/bootstrap.min.css'
 import $  from    'jquery'
+import axios  from    'axios'
+
+const prodUrl = "http://localhost:8000/api/ProductsApi"
+
+const fetchProducts = () => {
+    try
+    {
+        const prodsFromDb =  axios.get(prodUrl)   
+        return prodsFromDb.data
+
+    }
+    catch(e)
+    {
+        console.log(e)
+    }
+   
+
+}
 
 $(function(){
-    $('#btnGetProducts').on('click',() => {
-        alert('got it')
+    $('#btnGetProducts').on('click', ()   => {
+        const prods =   fetchProducts()
+        console.log(prods )
     })
 })
